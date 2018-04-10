c532a9f752c0cfba8262ab4023cb049e65a78b41
KBC11605156\kbc11605156 <eisuke-t-16d@kbc.ac.jp>
2018-04-10 15:09:42 +0900

    色々と変更

---
+++ Yuraves/Assets/Taira/Script/Director.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        QualitySettings.vSyncCount = 0;     //VSyncをOFFにする
+        Application.targetFrameRate = 60;   //ターゲットフレームレート
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
