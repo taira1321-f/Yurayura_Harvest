523de48858b77ec156fe65e7c1d16b513c00a937
KBC11605156\kbc11605156 <eisuke-t-16d@kbc.ac.jp>
2018-04-24 15:44:50 +0900

    ゲームメインのUI作成

--- Yuraves/Assets/Taira/Script/Director.cs
+++ Yuraves/Assets/Taira/Script/Director.cs
@@ -5,29 +5,46 @@ using UnityEngine.UI;
 
 public class Director : MonoBehaviour {
 
-    public GameObject TimeGage;
-    Slider slider;
-    float CountTime = 0;
-    const float LimitTime = 60.0f;
-    bool TimeFlg = true;
+    enum MODE { PLAY, STAY };
+    MODE gameMode;
+    
+    public static float CountTime = 0;
+    public static int Score;
 
 	// Use this for initialization
 	void Start () {
         QualitySettings.vSyncCount = 0;     //VSyncをOFFにする
         Application.targetFrameRate = 60;   //ターゲットフレームレート
-        slider = TimeGage.GetComponent<Slider>();
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (TimeFlg) CountTime += Time.deltaTime;
-        if (CountTime >= LimitTime) CountTime = 0;
-        slider.value = CountTime / LimitTime;
+        if (gameMode == MODE.PLAY) CountTime += Time.deltaTime;
+
+        switch (gameMode){
+            case MODE.PLAY:
+                DebugScore();
+                break;
+            case MODE.STAY:
+                Debug.Log("ステイ");
+                break;
+        }
+        DebugChg();
 	}
+    void DebugChg() {
+        if (Input.GetKeyDown(KeyCode.Z)) gameMode = MODE.PLAY;
+        if (Input.GetKeyDown(KeyCode.X)) gameMode = MODE.STAY;
+    }
 
     public void MenuButton() {
         Debug.Log("押されたよ");
-        if (TimeFlg) TimeFlg = false;
-        else TimeFlg = true;
+        if (gameMode == MODE.PLAY) gameMode = MODE.STAY;
+        else if (gameMode == MODE.STAY) gameMode = MODE.PLAY;
+    }
+
+    void DebugScore() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Score += 1;
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) Score -= 1;
     }
 }
---
+++ Yuraves/Assets/Taira/Script/PauseDirector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseDirector : MonoBehaviour {
+
+    public GameObject[] SelectB;
+    bool PauseFlg;
+
+	// Use this for initialization
+	void Start () {
+        PauseFlg = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PauseFlg) {
+            GameObject go = SelectB[0].transform.Find("Stop").gameObject;
+            go.SetActive(true);
+            Debug.Log("ウェーイ");
+        }
+
+	}
+
+    void flgChg() {
+        
+    }
+}
