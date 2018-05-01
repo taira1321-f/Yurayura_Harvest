252c0eedb10ee3e722a4f956cb1c110747d11cda
DESKTOP-0ARPSR5\玉那覇臣 <tamanaha1205@gmail.com>
2018-05-01 09:59:28 +0900

    フリック途中

---
+++ Yuraves/Assets/Jin/Script/Flick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flick : MonoBehaviour {
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown() {
+        //このオブジェクトの位置(transform.position)をスクリーン座標に変換
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        //ワールド座標上の、マウスカーソルと、対象の位置の差分
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseUp() {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
