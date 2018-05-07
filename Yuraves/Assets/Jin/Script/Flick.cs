2eb677fa7ec8b71442f02c9b67935a6084e9ddc9
DESKTOP-0ARPSR5\玉那覇臣 <tamanaha1205@gmail.com>
2018-05-07 11:24:25 +0900

    test2

--- Yuraves/Assets/Jin/Script/Flick.cs
+++ Yuraves/Assets/Jin/Script/Flick.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flick : MonoBehaviour {
+    //定数
+    private const float MouseDistanceY = -1.5f;
+        //変数
+    [SerializeField]
+    private GameObject Player;
 
     private Vector3 screenPoint;
     private Vector3 offset;
+
+     void Start () {
+		 Player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+
 
     void OnMouseDown() {
         //このオブジェクトの位置(transform.position)をスクリーン座標に変換
@@ -13,15 +24,16 @@ public class Flick : MonoBehaviour {
         //ワールド座標上の、マウスカーソルと、対象の位置の差分
         offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
-
-    void OnMouseUp() {
-        
+    void OnMouseDrag() {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y-30f, screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.position = currentPosition;
     }
-
-    // Use this for initialization
-    void Start () {
-		
-	}
+    void SetParent()
+    {
+        gameObject.transform.parent = Player.transform;
+        gameObject.transform.position += new Vector3(0.0f, MouseDistanceY, 0.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Yuraves/Assets/Jin/Script/ManCon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManCon : MonoBehaviour {
+   
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision) {
+        //衝突判定
+        if (collision.gameObject.tag == "Spring") {
+            //相手のタグがSpringならば、自分を消す
+            Destroy(this.gameObject);
+        }
+    }
+}
