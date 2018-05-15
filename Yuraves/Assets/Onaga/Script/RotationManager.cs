c9975fec31f4b1a3199ae642574ab9be59494f86
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2018-05-15 16:08:55 +0900

    揺れる際の下限と上限を設定

--- Yuraves/Assets/Onaga/Script/RotationManager.cs
+++ Yuraves/Assets/Onaga/Script/RotationManager.cs
@@ -7,6 +7,10 @@ public class RotationManager : MonoBehaviour
     private const float MaxVector = 2.5f;
     private const float MoveSpeed = 2.88f;
     private const float VectorOne = 0.25f;
+    private const float MaxRotation = 120.0f;
+    private const float MinRotation = 240.0f;
+    private const float StopperRotation = 180.0f;
+
     public enum ChildCount { none, child };
     public static ChildCount childmode;
 
@@ -38,16 +42,33 @@ public class RotationManager : MonoBehaviour
             case ChildCount.child:
                 float SwayCount = MousePosition.TolVector / VectorOne;
                 float SwayLimit = SwayCount * MoveSpeed;
-
                 switch (MousePosition.XDMode)
                 {
                     case MousePosition.XDStatus.right:
-                        transform.eulerAngles -= new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
+                        Debug.Log("MinRotation" + MinRotation);
+                        Debug.Log("MaxRotation" + MaxRotation);
+                        Debug.Log("transform.eulerAngles.z" + transform.eulerAngles.z);
+                        if (MinRotation<transform.eulerAngles.z || MaxRotation >= (int)transform.eulerAngles.z)
+                        {
+                            transform.eulerAngles -= new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
+                            if(MinRotation >= transform.eulerAngles.z && StopperRotation <= transform.eulerAngles.z)
+                            {
+                                transform.eulerAngles = new Vector3(0, 0, MinRotation);
+                            }
+                        }
                         ReleaseChild();
                         break;
 
                     case MousePosition.XDStatus.left:
-                        transform.eulerAngles += new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
+                        Debug.Log("transform.eulerAngles.z" + transform.eulerAngles.z);
+                        if (MaxRotation > transform.eulerAngles.z || StopperRotation <= (int)transform.eulerAngles.z)
+                        {
+                            transform.eulerAngles += new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
+                            if (MaxRotation <= transform.eulerAngles.z && StopperRotation >= transform.eulerAngles.z)
+                            {
+                                transform.eulerAngles = new Vector3(0, 0, MaxRotation);
+                            }
+                        }
                         ReleaseChild();
                         break;
                 }
