814ac04b1ef556af1df8fd48ea5c2c4bb0b5372d
LTKIYOHITO\yogikiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2018-05-17 16:30:40 +0900

    生成と成長処理

---
+++ Yuraves/Assets/Yogi/Calotte.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calotte : MonoBehaviour
+{
+    //定数
+    private const float ClickMaxTime = 1.0f;
+    private const float MouseDistanceY = -1.5f;
+    enum CalotteType { FLEE, KEEP };
+    //変数
+    [SerializeField]
+    private bool rigidbodyflg;
+    [SerializeField]
+    private GameObject Player;
+    public bool KeepFlg;
+    CalotteType ctype;
+    [SerializeField]
+    private float ClickTime;
+
+
+    //以下の関数はすべてprivateなので省略します。
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        ClickTime = 0.0f;
+        KeepFlg = false;
+    }
+
+    void Initialize()
+    {
+        ClickTime = 0.0f;
+        KeepFlg = false;
+    }
+
+    void Update()
+    {
+        switch (ctype)
+        {
+            case CalotteType.FLEE:
+                if (Input.GetMouseButton(0))
+                {
+                    if (KeepFlg)
+                    {
+                        ClickTime += Time.deltaTime;
+                        if (ClickTime >= ClickMaxTime)
+                        {
+                            SetParent();
+                            ctype = CalotteType.KEEP;
+                        }
+                    }
+                }
+                break;
+
+            case CalotteType.KEEP:
+
+                if (Input.GetMouseButtonUp(0))
+                {
+                    NoneParent();
+                    ctype = CalotteType.FLEE;
+                }
+                break;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            KeepFlg = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Initialize();
+        }
+    }
+
+    void SetParent()
+    {
+        gameObject.transform.parent = Player.transform;
+        gameObject.transform.position += new Vector3(0.0f, MouseDistanceY, 0.0f);
+    }
+    void NoneParent()
+    {
+        gameObject.transform.parent = null;
+        if (rigidbodyflg)
+        {
+            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
