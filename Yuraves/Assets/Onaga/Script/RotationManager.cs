4b2c011f3526f1dbd2262d4804250deefca2f552
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2018-05-23 14:36:27 +0900

    揺れ戻り仮実装

--- Yuraves/Assets/Onaga/Script/RotationManager.cs
+++ Yuraves/Assets/Onaga/Script/RotationManager.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class RotationManager : MonoBehaviour
 {
-    private const float MaxVector = 2.5f;
-    private const float MoveSpeed = 2.88f;
-    private const float VectorOne = 0.25f;
-    private const float MaxRotation = 120.0f;
-    private const float MinRotation = 240.0f;
-    private const float StopperRotation = 180.0f;
+    private const float MaxVector = 2.5f; //移動スピード上限
+    private const float MoveSpeed = 2.88f;//傾きのスピード
+    //private const float VectorOne = 0.25f;
+    private const float MaxRotation = 120.0f;//傾きの上限
+    private const float MinRotation = 240.0f;//傾きの下限
+    private const float StopperRotation = 180.0f;//傾きの判定用の数値
 
-    public enum ChildCount { none, child };
+    private const float ZeroSet = 10.0f;//動いていないときの戻る間隔
+    private const float InitSpeed = 5.0f;//動かしていないときの戻る速さ
+
+    public enum ChildCount { none, child };//子供がいるかいないか
+    private enum PositionInit { right, left, zero };//移動方向が右か左か動いていないか
     public static ChildCount childmode;
+    private PositionInit positionInit;
+    private float InitVactor;//動いていないときの戻る数値
+    private float VectorCounter;
+    private float saveVector;
 
     void Start()
     {
-
+        InitVactor = 0.0f;
     }
 
+    void Initialize()
+    {
+        InitVactor = 0.0f;
+    }
     void Update()
     {
         if (this.transform.childCount == 0)
@@ -36,31 +48,31 @@ public class RotationManager : MonoBehaviour
     {
         switch (childmode)
         {
+            //子供がいない
             case ChildCount.none:
                 transform.eulerAngles += new Vector3(0, 0, 0);
                 break;
+            //子供がいる
             case ChildCount.child:
-                float SwayCount = MousePosition.TolVector / VectorOne;
-                float SwayLimit = SwayCount * MoveSpeed;
+                //float SwayCount = MousePosition.TolVector / VectorOne;
+                //float SwayLimit = SwayCount * MoveSpeed;
                 switch (MousePosition.XDMode)
                 {
+                    //右に移動中
                     case MousePosition.XDStatus.right:
-                        Debug.Log("MinRotation" + MinRotation);
-                        Debug.Log("MaxRotation" + MaxRotation);
-                        Debug.Log("transform.eulerAngles.z" + transform.eulerAngles.z);
-                        if (MinRotation<transform.eulerAngles.z || MaxRotation >= (int)transform.eulerAngles.z)
+                        if (MinRotation < transform.eulerAngles.z || MaxRotation >= (int)transform.eulerAngles.z)
                         {
                             transform.eulerAngles -= new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
-                            if(MinRotation >= transform.eulerAngles.z && StopperRotation <= transform.eulerAngles.z)
+                            if (MinRotation >= transform.eulerAngles.z && StopperRotation <= transform.eulerAngles.z)
                             {
                                 transform.eulerAngles = new Vector3(0, 0, MinRotation);
                             }
                         }
+                        SlopeVector(transform.eulerAngles.z);
                         ReleaseChild();
                         break;
-
+                    //左に移動中
                     case MousePosition.XDStatus.left:
-                        Debug.Log("transform.eulerAngles.z" + transform.eulerAngles.z);
                         if (MaxRotation > transform.eulerAngles.z || StopperRotation <= (int)transform.eulerAngles.z)
                         {
                             transform.eulerAngles += new Vector3(0, 0, MousePosition.TolVector * MoveSpeed);
@@ -69,7 +81,58 @@ public class RotationManager : MonoBehaviour
                                 transform.eulerAngles = new Vector3(0, 0, MaxRotation);
                             }
                         }
+                        SlopeVector(transform.eulerAngles.z);
                         ReleaseChild();
+                        break;
+
+                    //移動していない
+                    case MousePosition.XDStatus.initial:
+                        int roopcount = 1;
+                        if (VectorCounter > 0.0f)
+                        {
+                            switch (positionInit)
+                            {
+                                case PositionInit.left:
+                                    //Debug.Log(roopcount * ZeroSet);
+                                    if (VectorCounter > roopcount * (int)ZeroSet)
+                                    {
+                                        transform.eulerAngles += new Vector3(0, 0, InitSpeed);
+                                        VectorCounter -= InitSpeed;
+                                    }
+                                    else
+                                    {
+                                        roopcount++;
+                                        saveVector -= ZeroSet;
+                                        VectorCounter = saveVector;
+                                        positionInit = PositionInit.right;
+                                    }
+                                    break;
+
+                                case PositionInit.right:
+                                    if (VectorCounter > roopcount * (int)ZeroSet)
+                                    {
+                                        transform.eulerAngles -= new Vector3(0, 0, InitSpeed);
+                                        VectorCounter -= InitSpeed;
+                                    }
+                                    else
+                                    {
+                                        roopcount++;
+                                        saveVector -= ZeroSet;
+                                        VectorCounter = saveVector;
+                                        positionInit = PositionInit.left;
+                                    }
+                                    break;
+                            }
+
+                            if (VectorCounter - roopcount * ZeroSet < ZeroSet)
+                            {
+
+                                Debug.Log("通った");
+                                roopcount = 0;
+                                InitVactor = 0.0f;
+                                transform.eulerAngles -= new Vector3(0, 0, 0);
+                            }
+                        }
                         break;
                 }
                 break;
@@ -85,5 +148,36 @@ public class RotationManager : MonoBehaviour
                 child.GetComponent<Mandragora>().ctype = Mandragora.CalotteType.RESET;
             }
         }
+    }
+
+    void SlopeVector(float Vector)
+    {
+        Debug.Log("通ってる");
+        if (Vector >= MinRotation)
+        {
+            positionInit = PositionInit.left;
+        }
+        else if (Vector <= MaxRotation)
+        {
+            positionInit = PositionInit.right;
+        }
+        else if (Vector == 0.0f)
+        {
+            positionInit = PositionInit.zero;
+        }
+
+        switch (positionInit)
+        {
+            case PositionInit.left:
+                InitVactor = 360.0f - transform.eulerAngles.z;
+                break;
+            case PositionInit.right:
+                InitVactor = transform.eulerAngles.z;
+                break;
+            case PositionInit.zero:
+                InitVactor = 0.0f;
+                break;
+        }
+        VectorCounter = saveVector = InitVactor * 2;
     }
 }
