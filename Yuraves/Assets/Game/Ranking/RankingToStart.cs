e9da19b2fad92aac1a841bb5dc70081b9b882b2b
LTKIYOHITO\yogikiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2018-06-08 13:17:57 +0900

    一時保存

---
+++ Yuraves/Assets/Game/Ranking/RankingToStart.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RankingToStart : FadeScript
+{
+
+    int ToStartFlg = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        FadeInFlg = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeOutSet(FadeOutFlg);
+        FadeInSet(FadeInFlg);
+
+        if (ToStartFlg == 1 && A >= 1.0f)
+        {
+            SceneManager.LoadScene("MainScene");//シーン移動
+
+        }
+
+        if (A <= 0.0f)
+        {
+
+            FadeInFlg = false;
+        }
+
+
+    }
+
+    public void SceneChange()
+    {
+        FadeOutFlg = true;
+        ToStartFlg = 1;
+
+        //SceneManager.LoadScene("DummyScene");
+    }
+    //かわいいよー
+}
