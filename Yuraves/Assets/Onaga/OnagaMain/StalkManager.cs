18d54413e06421f4087725438dd93229b56804f2
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2018-06-08 12:51:32 +0900

    茎追加

---
+++ Yuraves/Assets/Onaga/OnagaMain/StalkManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalkManager : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;//自身のスプライトレンダーを入れるスプライト変数
+    public GameObject RotationManager;//ローテーションマネージャーをいれるやつ
+    public GameObject TargetObject;//ローテーションマネージャーの子供を検索していれるやつ
+
+    //スプライト入れる変数-----------
+    public Sprite YoungMandragora;
+    public Sprite MiddleMandragora;
+    public Sprite Oldimage2;
+    //-------------------------------
+    [SerializeField]
+    private int MandragoraState;//TargetObjectのObjectMode取得用の変数
+    // Use this for initialization
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        TargetObject = null;
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TargetObject = RotationManager.transform.FindChild("Child").gameObject;
+        if (RotationManager.transform.childCount == 0)
+        {
+            TargetObject = null;
+        }
+        foreach (Transform transform in RotationManager.transform)
+        {
+            // Transformからゲームオブジェクト取得・削除
+            if (transform.gameObject != null)
+            {
+                TargetObject = transform.gameObject;
+            }
+        }
+
+        if (TargetObject != null)
+        {
+            MandragoraState = TargetObject.GetComponent<Yogi.ChangeObjectMode>().ObjectMode;
+
+            switch (MandragoraState)
+            {
+                case 1:
+                    spriteRenderer.sprite = YoungMandragora;
+                    break;
+
+                case 2:
+                    spriteRenderer.sprite = MiddleMandragora;
+                    break;
+
+            }
+        }
+        else
+        {
+            spriteRenderer.sprite = null;
+        }
+    }
+}
