1ad1d6337f50224fda6bb07d00071e508aca38d7
LTKIYOHITO\yogikiyohito <kiyohito-y-17d@stu.kbc.ac.jp>
2018-06-20 15:20:49 +0900

    一時保存

--- Yuraves/Assets/Game/Ranking/RankingLoad.cs
+++ Yuraves/Assets/Game/Ranking/RankingLoad.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class RankingLoad : MonoBehaviour
 {
 
-    //　読み込んだテキストを出力するUIテキスト
+
     [SerializeField]
     public int Score1;
     public int Score2;
@@ -15,36 +15,16 @@ public class RankingLoad : MonoBehaviour
     public int Score5;
     public int Score;
 
-    
-    //　読む込むテキストが書き込まれている.txtファイル
-    [SerializeField]
-    private TextAsset textAsset;
-    //　テキストファイルから読み込んだデータ
-    private string loadText1;
-    //　Resourcesフォルダから直接テキストを読み込む
-    private string loadText2;
-    //　改行で分割して配列に入れる
-    private string[] splitText1;
-    //　改行で分割して配列に入れる
-    private string[] splitText2;
-
-
     void Start()
     {
-        loadText1 = textAsset.text;
-        splitText1 = loadText1.Split(char.Parse("\n"));
+        Score1 = PlayerPrefs.GetInt("RankingNumber1", 1234);
+        Score2 = PlayerPrefs.GetInt("RankingNumber2", 1234);
+        Score3 = PlayerPrefs.GetInt("RankingNumber3", 1234);
+        Score4 = PlayerPrefs.GetInt("RankingNumber4", 1234);
+        Score5 = PlayerPrefs.GetInt("RankingNumber5", 1234);
 
-        Score1 = int.Parse(splitText1[0]);
-        Score2 = int.Parse(splitText1[1]);
-        Score3 = int.Parse(splitText1[2]);
-        Score4 = int.Parse(splitText1[3]);
-        Score5 = int.Parse(splitText1[4]);
-        //Score1 = 1234;
-        //Score2 = 4568;
-        //Score3 = 1245;
-        //Score4 = 8764;
-        //Score5 = 9875;
-        //dataText.text = "マウスの左クリックで通常のテキストファイルの読み込み、右クリックでResourcesフォルダ内のテキストファイル読み込みしたテキストが表示されます。";
+        Debug.Log("Score1="+Score1);
+       
     }
 
     void Update()
