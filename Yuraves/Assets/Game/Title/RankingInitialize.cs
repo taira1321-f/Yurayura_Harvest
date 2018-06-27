21c40fb91ba72fdce1a0c2f7f673b9b754a68f89
KBC11605156\kbc11605156 <eisuke-t-16d@kbc.ac.jp>
2018-06-27 15:22:02 +0900

    フェードバグを解決

--- Yuraves/Assets/Game/Title/RankingInitialize.cs
+++ Yuraves/Assets/Game/Title/RankingInitialize.cs
@@ -7,16 +7,10 @@ public class RankingInitialize : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        Debug.Log(PlayerPrefs.GetInt("RankingNumber1", 0000));
-        Debug.Log(PlayerPrefs.GetInt("RankingNumber2", 0000));
-        Debug.Log(PlayerPrefs.GetInt("RankingNumber3", 0000));
-        Debug.Log(PlayerPrefs.GetInt("RankingNumber4", 0000));
-        Debug.Log(PlayerPrefs.GetInt("RankingNumber5", 0000));
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    static void RankingInit()
-    {
-        PlayerPrefs.DeleteAll();
+    static void RankingInit(){
+      //  PlayerPrefs.DeleteAll();
     }
 }
