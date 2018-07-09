56e89214b742a9930bc3a7cda41e56f9a327e19e
DESKTOP-0ARPSR5\jin <tamanaha1205@gmail.com>
2018-07-09 11:03:09 +0900

    test

---
+++ Yuraves/Assets/Jin/Script/JinJinJin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JinJinJin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("ちんこちんこおっぱい");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
